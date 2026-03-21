c271e9ffdf5977c2c90e2b29ea017902f84376ee
Wkotau <platon.kotau@gmail.com>
2026-03-21 08:54:05 +0100

    add styles

--- src/Notes/Notes.jsx
+++ src/Notes/Notes.jsx
@@ -56,46 +56,51 @@ export default function Notes({setLogin}) {
 
 
     return (
-        <div className="">
-            <br />
-            <div className="flex gap-1">
-            <input  className='bg-white rounded-sm border-0.5 p-1' type="text" value={title} placeholder="title" onChange={(e) => setTitle(e.target.value)} />
-            <br />
-            <input className='bg-white rounded-sm border-0.5 p-1' type="text" value={desc} placeholder="description" onChange={(e) => setDesc(e.target.value)} />
-            
-            <button  className='bg-white rounded-sm p-3 active:dark:bg-blue-300 hover:dark:bg-blue-100' onClick={addNote}>+</button>
+        <div>
+            <div className="border border-gray-300 px-10 my-3 rounded-sm bg-gray-50 min-h-screen shadow-md" >
+                    <br />
+                <div className="flex gap-1 ">
+                    <input  className='bg-white border border-gray-300 rounded-sm px-3 py-2' type="text" value={title} placeholder="title" onChange={(e) => setTitle(e.target.value)} />
+                    <br />
+                    <input className='bg-white border border-gray-300 rounded-sm px-3 py-2' type="text" value={desc} placeholder="description" onChange={(e) => setDesc(e.target.value)} />
+
+                    <button  className='border border-gray-300 bg-white rounded-sm p-3 active:dark:bg-blue-300 hover:dark:bg-blue-100 text-gray-500' onClick={addNote}>+</button>
+                </div>
+                    <br />
+
+
+
+
+                {notes.map(note => (
+                    <div key={note.id} onClick={() => {
+                        setEdit(note.id)
+                        setEtitle(note.name)
+                        setEdesc(note.descripiton)
+                            }} >
+                        <div className="border border-gray-300 bg-white rounded-sm p-3 flex flex-col my-2 max-w-l">
+                            <div className=" flex justify-baseline flex-col max-w-l">
+                                <h1 className="text-l font-semibold mb-0 leading-tight ">{note.name}</h1>
+                                <p className="text-sm font-normal mt-0">{note.descripiton}</p>
+                            </div>
+
+                            <div className="flex justify-end">
+                                <button className=' border border-gray-300 bg-white rounded-sm px-3 py-1 m-2 active:dark:bg-blue-300 hover:dark:bg-blue-100 text-gray-500' onClick={() => deleteNote(note.id)}>-</button>
+                            </div>
+                                
+
+                            {edit === note.id && (
+                            <div className='flex justify-center items-center' onClick={(e) => e.stopPropagation()}>
+                                <input className="'bg-white border border-gray-300 rounded-sm px-3 py-2 mx-1" value={Etitle} onChange={(e) => setEtitle(e.target.value)} />
+                                <input className="'bg-white border border-gray-300 rounded-sm px-3 py-2 mx-1" value={Edesc} onChange={(e) => setEdesc(e.target.value)} />
+                                <button className=' border border-gray-300 bg-white rounded-sm px-3 py-2 m-2 active:dark:bg-blue-300 hover:dark:bg-blue-100 text-gray-500' onClick={updateNote}>Update</button>
+                            </div>)}
+                        </div>
+                    </div>))} 
+                        <br />
+                </div>
+                                <div className="flex justify-around">
+                                    <button className= " bg-white rounded-sm px-3 py-2 m-2 active:dark:bg-blue-600 active:text-white  hover:dark:bg-blue-100 shadow-md" onClick={() => logout()}>LogOut</button>
+                                </div>
             </div>
-            <br />
-            
-            
-
-
-            {notes.map(note => (
-                <div key={note.id} onClick={() => {
-                    setEdit(note.id)
-                    setEtitle(note.name)
-                    setEdesc(note.descripiton)
-                        }} >
-                <div className="border-2 p-1 rounded-md flex justify-between items-center">
-                    <div>
-                        <h1 className="text-xl font-bold">{note.name}</h1>
-                        <p className="text-sm font-light">{note.descripiton}</p>
-                    </div>
-                <button className='bg-white rounded-sm px-3 py-1 m-2 active:dark:bg-blue-300 hover:dark:bg-blue-100' onClick={() => deleteNote(note.id)}>-</button>
-                </div>
-
-                {edit === note.id && (
-                    <div onClick={(e) => e.stopPropagation()}>
-                        <input className="bg-white rounded-sm border-0.5 p-1 mx-1" value={Etitle} onChange={(e) => setEtitle(e.target.value)} />
-                        <input className="bg-white rounded-sm border-0.5 p-1 mx-1" value={Edesc} onChange={(e) => setEdesc(e.target.value)} />
-                        <button className='bg-white rounded-sm px-2 py-1 m-1 active:dark:bg-blue-300 hover:dark:bg-blue-100' onClick={updateNote}>Update</button>
-                    </div>
-                )}
-                </div>
-            ))}
-
-            <br />
-        <button className="bg-white p-2 rounded-md active:dark:bg-blue-300 hover:dark:bg-blue-100" onClick={() => logout()}>LogOut</button>
-        </div>
     )
 }
